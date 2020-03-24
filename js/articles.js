68ca8d64a192821179f2dbc97c2d1e075fc9ddf8
VoicaLucian09 <voica.lucian94@gmail.com>
2020-03-24 02:18:52 +0200

    refactored data

--- js/articles.js
+++ js/articles.js
@@ -1,31 +1,37 @@
-﻿var articles = [
-    {
-	    banner:"img/elvetia/banner.jpg",
-        title: "Elveția: simplitate și tehnologie",
-        date: "Februarie 2019",
-        category: "Europa",
-        page: "~/elvetia.html",
-        description: "Opt zile, sute de mii de pași, trenuri, ciocolată, din inima orașelor tehnologizate în colțuri natură parcă uitate. Stereotipuri, clișee, bani și brânză. Harta traseului parcurs în încercarea de a nu lăsa nimic nedescoperit."
-    },
-    [
-        {
-	        banner:"img/elvetia/banner.jpg",
-            title: "Elveția: simplitate și tehnologie",
-            date: "Februarie 2019",
-            category: "Europa",
-            page: "~/elvetia.html",
-            description: "Opt zile, sute de mii de pași, trenuri, ciocolată, din inima orașelor tehnologizate în colțuri natură parcă uitate. Stereotipuri, clișee, bani și brânză. Harta traseului parcurs în încercarea de a nu lăsa nimic nedescoperit."
-        },
-        {
-	        banner:"img/elvetia/banner.jpg",
-            title: "Elveția: simplitate și tehnologie",
-            date: "Februarie 2019",
-            category: "Europa",
-            page: "~/elvetia.html",
-            description: "Opt zile, sute de mii de pași, trenuri, ciocolată, din inima orașelor tehnologizate în colțuri natură parcă uitate. Stereotipuri, clișee, bani și brânză. Harta traseului parcurs în încercarea de a nu lăsa nimic nedescoperit."
-        }
-    ]
-];
+﻿(function(){
+    var eElement = document.getElementById("articles-div");
+
+    var art = GetArticles();
+    art.forEach(function (item) {
+        if (item.child.length == 1) {
+            var article = GenerateArticle(item.child[0]);
+            eElement.innerHTML += article;
+        } else {
+        var element
+            var element = '<div class="row">';
+            item.child.forEach(function (smallArticle) {
+                var smallArticle = GenerateArticle(smallArticle, true);
+                element += smallArticle;
+            });
+            element += '</div>';
+            eElement.innerHTML += element;
+		}
+    });
+})();
+
+function GenerateArticle(item, small = false) {
+        var article = "";
+        if(small) { 
+            article = GetSmallArticleTemplate();
+        } else {
+            article = GetArticleTemplate();
+		}
+        var variables = getWordsBetweenCurlies(article);
+        variables.forEach(function(variable) {
+            article = article.replace("{"+variable+"}", item[variable]);
+        });
+        return article;
+}
 function GetArticleTemplate() {
     return '<article class="blog_style1"> \
                 <div class="blog_img"> \
@@ -36,7 +42,7 @@ function GetArticleTemplate() {
 						<div class="cat">\
 							<a class="cat_btn" href="#">Citește</a>\
 							<a href="#"><i class="fa fa-calendar" aria-hidden="true"></i> {date}</a>\
-							<a href="#"><i class="fa fa-map-pin" aria-hidden="true"></i> {category}</a>\
+							<a href="#"><i class="fa fa-map-pin" aria-hidden="true"></i> {country} - {category}</a>\
 						</div>\
 						<a href="#"><h4>{title}</h4></a>\
                         <p>{description}</p>\
@@ -56,7 +62,7 @@ function GetSmallArticleTemplate() {
 						    <div class="cat">\
 							    <a class="cat_btn" href="#">Citește</a>\
 							    <a href="#"><i class="fa fa-calendar" aria-hidden="true"></i> {date}</a>\
-							    <a href="#"><i class="fa fa-map-pin" aria-hidden="true"></i> {category}</a>\
+							    <a href="#"><i class="fa fa-map-pin" aria-hidden="true"></i> {country}</a>\
 						    </div>\
 						    <a href="#"><h4>{title}</h4></a>\
                             <p>{description}</p>\
@@ -66,49 +72,6 @@ function GetSmallArticleTemplate() {
                 </article>\
             </div>';
 }
-
-(function(){
-    var eElement = document.getElementById("articles-div");
-    articles.forEach(function(item) { 
-        if(!Array.isArray(item)) {
-            var article = GenerateArticle(item);
-            
-            eElement.insertBefore(htmlToElement(article), eElement.firstChild);
-        } else {
-        var element
-            var element = '<div class="row">';
-            item.forEach(function(smallArticle) {
-                var smallArticle = GenerateArticle(smallArticle, true);
-                element += smallArticle;
-            });
-            element += '</div>';
-            eElement.insertBefore(htmlToElement(element), eElement.firstChild);
-		}
-        
-    });
-})();
-
-function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim();
-    template.innerHTML = html;
-    return template.content.firstChild;
-}
-
-function GenerateArticle(item, small = false) {
-        var article = "";
-        if(small) { 
-            article = GetSmallArticleTemplate();
-        } else {
-            article = GetArticleTemplate();
-		}
-        var variables = getWordsBetweenCurlies(article);
-        variables.forEach(function(variable) {
-            article = article.replace("{"+variable+"}", item[variable]);
-        });
-        return article;
-}
-
 function getWordsBetweenCurlies(str) {
   var results = [], re = /{([^}]+)}/g, text;
 
---
+++ js/repo.js
@@ -0,0 +1,63 @@
+﻿var articleLines = [
+    { id: 1, order: 1 },
+    { id: 2, order: 2 }
+];
+
+var countries = [
+    { id: 1, name: "Elveția" },
+    { id: 2, name: "România" }
+];
+
+var articles = [
+    {
+        id: 1,
+        banner: "img/elvetia/banner.jpg",
+        title: "Elveția: simplitate și tehnologie",
+        date: "Februarie 2019",
+        countryId: 1,
+        articleLineId: 1,
+        category: "Europa",
+        page: "~/elvetia.html",
+        description: "Opt zile, sute de mii de pași, trenuri, ciocolată, din inima orașelor tehnologizate în colțuri natură parcă uitate. Stereotipuri, clișee, bani și brânză. Harta traseului parcurs în încercarea de a nu lăsa nimic nedescoperit."
+    },
+    {
+        id: 2,
+        banner: "img/elvetia/banner.jpg",
+        title: "Elveția: simplitate și tehnologie",
+        date: "Februarie 2019",
+        countryId: 1,
+        articleLineId: 2,
+        category: "Europa",
+        page: "~/elvetia.html",
+        description: "Opt zile, sute de mii de pași, trenuri, ciocolată, din inima orașelor tehnologizate în colțuri natură parcă uitate. Stereotipuri, clișee, bani și brânză. Harta traseului parcurs în încercarea de a nu lăsa nimic nedescoperit."
+    },
+    {
+        id: 3,
+        banner: "img/elvetia/banner.jpg",
+        title: "Elveția: simplitate și tehnologie",
+        date: "Februarie 2019",
+        countryId: 1,
+        articleLineId: 2,
+        category: "Europa",
+        page: "~/elvetia.html",
+        description: "Opt zile, sute de mii de pași, trenuri, ciocolată, din inima orașelor tehnologizate în colțuri natură parcă uitate. Stereotipuri, clișee, bani și brânză. Harta traseului parcurs în încercarea de a nu lăsa nimic nedescoperit."
+    }
+];
+
+function GetArticles() {
+    var fullArt = [];
+    articles.forEach(function (item) {
+        item.country = countries.filter(obj => {
+            return obj.id === item.countryId
+        })[0].name;
+        fullArt.push(item);
+    });
+
+    articleLines.forEach(function (articleLine) {
+        articleLine.child = articles.filter(obj => {
+            return obj.articleLineId === articleLine.id
+        });
+    });
+
+    return articleLines.sort((a, b) => (a.order > b.order) ? 1 : -1);
+}
